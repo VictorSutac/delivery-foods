584b72e6d7da87dec56ba64531f90f91f54743ee
Victor Sutac <myspry@icloud.com>
2025-09-18 11:29:53 +0300

    Обновил 4й урок с домашкой

--- scripts/index.js
+++ scripts/index.js
@@ -135,5 +135,110 @@ const restsFunc = () => {
   }
 };
 
+const goodsFunc = () => {
+  const container = document.querySelector("#goods-container");
+  const goodsArray = [
+    {
+      id: 0,
+      name: "Ролл угорь стандарт",
+      price: 250,
+      text: "Рис, угорь, соус унаги, кунжут, водоросли нори.",
+      image: "goods-1.jpg",
+    },
+    {
+      id: 1,
+      name: "Калифорния лосось стандарт",
+      price: 395,
+      text: "Рис, лосось, авокадо, огурец, майонез, икра масаго, водоросли нори.",
+      image: "goods-2.jpg",
+    },
+    {
+      id: 2,
+      name: "Окинава стандарт",
+      price: 250,
+      text: " Рис, креветка отварная, сыр сливочный, лосось, огурец свежий...",
+      image: "goods-3.jpg",
+    },
+    {
+      id: 3,
+      name: "Цезарь маки хl",
+      price: 250,
+      text: "Рис, куриная грудка копченая, икра масаго, томат, айсберг, соус цезарь...",
+      image: "goods-4.jpg",
+    },
+    {
+      id: 4,
+      name: "Ясай маки стандарт 185 г",
+      price: 250,
+      text: "Рис, помидор свежий, перец болгарский, авокадо, огурец, айсберг",
+      image: "goods-5.jpg",
+    },
+    {
+      id: 5,
+      name: "Ролл с креветкой стандарт",
+      price: 250,
+      text: "Рис, водоросли нори, креветки отварные, сыр сливочный, огурцы",
+      image: "goods-6.jpg",
+    },
+  ];
+
+  const loading = () => {
+    container.innerHTML =
+      '<p style="width:100%; text-align: center;">Загрузка</p>';
+  };
+
+  const randerGoods = (array) => {
+    container.innerHTML = "";
+
+    array.forEach((card) => {
+      container.insertAdjacentHTML(
+        "beforeend",
+        `
+        <div class="products-card">
+                <div class="products-card__image">
+                  <img src="./img/goods/${card.image}" alt="${card.image}" />
+                </div>
+                <div class="products-card__description">
+                  <div class="products-card__description-row">
+                    <h5 class="products-card__description--name">
+                      Ролл угорь стандарт
+                    </h5>
+                  </div>
+                  <div class="products-card__description-row">
+                    <p class="products-card__description--text">
+                      ${card.text}
+                    </p>
+                  </div>
+                  <div class="products-card__description-row">
+                    <div class="products-card__description-controls">
+                      <button class="btn btn-primary">
+                        В корзину
+                        <img
+                          src="./img/icons/shopping-cart-white.svg"
+                          alt="shopping-cart"
+                        />
+                      </button>
+                      <span class="products-card__description-controls--price">
+                        ${card.price} ₽
+                      </span>
+                    </div>
+                  </div>
+                </div>
+              </div>
+        `
+      );
+    });
+  };
+
+  loading();
+
+  if (container) {
+    setTimeout(() => {
+      randerGoods(goodsArray);
+    }, 1000);
+  }
+};
+
 modalFanc();
 restsFunc();
+goodsFunc();
